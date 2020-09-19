7a1e39eb85ee026b0d2e62d7223058bccd59ad88
Josh Brown <joshbrown2794@gmail.com>
2020-09-18 23:27:08 -0700

    psudo code only need to review local storage

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log(moment().format());
+console.log(moment().format("hA"))
+
+// TODO Create one row with time,  text area and button using JQ.
+
+
+// layout for time schedule includes time text area and save button
+{/* <div class="container">
+<div class="row time-block">
+<div class="col-md-2 hour">
+  9am
+</div>
+<!-- text area -->
+<textarea class="col-md-8"></textarea>
+<!-- save button -->
+<button class="saveBtn col-md-2">Save</button>
+</div>
+</div> */}
+
+// show the time on top of the calendar using moment. js grab the time and se the text of the p tag with the id of currenDay to be our time from moment.js
+
+//  TODO create an array to hold the hours that i need ["9am","10am"]
+
+// TODO USING A FOR LOOP
+
+// TODO LOOP OVER MY ARRAY OF HOURS AND CREATE A DIV FOR ROW, CREATE A DIV FOR HOURS, TEXTAREA AND A BUTTON
+
+// TODO GIVE THE ELEMENTS SOME CONTENT
+
+// TODO HOW CAN I COMPARE THE HOURS OF MY ROWS, TO THE CURRENT TIME? USE MOEMENY.JS GET THE CURRENT TIME AND COMPARE TOT THE TIME THAT IS BEING ASSINGED TO THE ROW
+
+//  TODO
